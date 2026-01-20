33ed3a1f8d09279070a74017c6bee894a9282809
Guilherme da Invenção Santos <gui.inven@gmail.com>
2026-01-20 20:55:34 -0300

    impedindo qualquer ação na imagem

--- app/page.tsx
+++ app/page.tsx
@@ -44,11 +44,12 @@ export default function Home() {
         </div>
 
         <Image
-          className="w-[100vw] absolute bottom-0 left-1/2 -translate-x-1/2"
+          className="w-[100vw] absolute bottom-0 left-1/2 -translate-x-1/2 pointer-events-none select-none"
           src="imgs/wave.svg"
           width={100}
           height={100}
           alt="imagem de onda"
+          draggable={false}
         />
       </section>
 
