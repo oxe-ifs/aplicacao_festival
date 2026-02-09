3984d95d88fd6e9234b314341efe1e1ca692e7e7
J. Herbert <jherbertsocial@gmail.com>
2026-02-09 10:47:29 -0300

    Adicionando Section cultura

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import Header from "@/components/Header";
+import PatrimonioImateCard from "@/components/PatrimonioImateCard";
 import ImportanciaCultCard from "@/components/ImportanciaCultCard"; 
 import ImportanciaEconCard from "@/components/ImportanciaEconCard";
 import ProgramacaoCard from "@/components/ProgramacaoCard";
@@ -70,27 +71,21 @@ export default function Home() {
         />
       </section>
 
-      <section id="noticias" className="xl:px-40 mt-20">
+      <section id="cultura" className="xl:px-40 mt-20">
         <div className="text-center justify-between">
-          <h1 className="text-3xl lg:text-5xl font-bold text-balance mb-5">Notícias e Comunicados</h1>
+          <h1 className="text-3xl lg:text-5xl font-bold text-balance mb-5">Cultura e História</h1>
           <p className="text-lg text-preto/60 text-balance">
-            Fique por dentro das últimas novidades dos Festejos
+            Conheça os patrimônios históricos e culturais da cidade
           </p>
+          <div className="mt-10 px-6 py-6 border border-preto/20 rounded-lg overflow-hidden shadow-md mx-5 md:mx-10 lg:mx-30 mb-10">
+            <div className="text-left text-md text-preto/95">
+              <p className="py-2">Com mais de 400 anos de história, Lagarto é um pilar de prosperidade e cultura em Sergipe. Fundada originalmente em 1604 no povoado Santo Antônio, a cidade tem suas raízes na expansão territorial e na fé católica. Após uma migração histórica no século XVII para a Colina do Lagarto — motivada pela superação de uma epidemia sob a bênção de Nossa Senhora da Piedade — o município consolidou sua vocação agropecuária e política.</p>
+              <p className="py-2">Elevada a Vila em 1697 e a Cidade em 1880, Lagarto construiu uma identidade forte e soberana. Esse orgulho é celebrado anualmente no dia 20 de outubro, o Dia da Lagartinidade, data que reafirma o protagonismo histórico, a religiosidade e a riqueza cultural que fazem de Lagarto uma referência no estado e no país.</p>
+            </div>
+          </div>
+          <h1 className="text-2xl lg:text-4xl font-bold">Tradições, Manifestações Culturais e Patrimônios imateriais</h1>
+          <PatrimonioImateCard />
         </div>
-        <NoticiasCard />
-        <div className="w-full text-center mt-8 mb-16">
-          <a href="#" className="text-sm font-semibold text-azul max-w-40 py-2 px-4 rounded-md border border-azul hover:bg-azul hover:text-branco">Ver Todas as Notícias</a>
-        </div>
-      </section>
-
-      <section id="sobre" className="xl:px-40 mt-20">
-        <div className="text-center justify-between xl:px-15 md:px-10 px-6">
-          <h1 className="text-3xl lg:text-5xl font-bold mb-5">Sobre os Festejos</h1>
-          <p className="text-lg text-preto/60 text-balance">
-          </p>
-          
-        </div>
-        <SobreCard />
       </section>
 
       <section id="mandioca" className="xl:px-40 mt-15">
@@ -105,7 +100,7 @@ export default function Home() {
           <div className="mt-10 px-6 py-6 border border-preto/20 rounded-lg overflow-hidden shadow-md mx-5 md:mx-10 lg:mx-30 mb-10">
             <div className="text-left text-md text-preto/95">
               <p className="py-2">O <strong>Festival da Mandioca</strong> em Lagarto, Sergipe, é uma das principais festas juninas do estado, valorizando a cultura local e a agricultura, criado em 2009 pelo ex-prefeito Valmir Monteiro. O evento celebra a mandioca, motor econômico regional, e inclui forró, cavalgadas e a tradicional Pega do Mastro.</p>
-              <p> O Festival é o coração dos Festejos Juninos de Lagarto, uma celebração que há décadas reúne a comunidade sergipana em torno de suas raízes culturais e gastronômicas. A mandioca, alimento essencial da cultura nordestina, é homenageada através de suas diversas formas e sabores.</p>
+              <p className="py-2"> O Festival é o coração dos Festejos Juninos de Lagarto, uma celebração que há décadas reúne a comunidade sergipana em torno de suas raízes culturais e gastronômicas. A mandioca, alimento essencial da cultura nordestina, é homenageada através de suas diversas formas e sabores.</p>
               <p className="py-2">Durante os festejos, a cidade se transforma em um grande palco de cores, sons e sabores. As tradicionais quadrilhas juninas, o forró pé-de-serra, as comidas típicas e o artesanato local criam uma atmosfera única que atrai milhares de visitantes de todo o Brasil.</p>
               <p className="py-2">Mais do que uma festa, os Festejos Juninos de Lagarto representam a preservação da identidade cultural sergipana e a valorização das tradições que passam de geração em geração, fortalecendo os laços comunitários e promovendo o turismo regional.</p>
             </div>
@@ -126,6 +121,16 @@ export default function Home() {
           </p>
         </div>
         <ProgramacaoCard />
+      </section>
+
+      <section id="agenda" className="xl:px-40 mt-20">
+        <div className="text-center justify-between xl:px-15 md:px-10 px-6">
+          <h1 className="text-3xl lg:text-5xl font-bold mb-5">Agenda Festiva</h1>
+          <p className="text-lg text-preto/60 text-balance">
+            Veja sobre as festividades e eventos culturais
+          </p>
+        </div>
+        
       </section>
 
       <section id="turismo" className="xl:px-40 mt-20">
